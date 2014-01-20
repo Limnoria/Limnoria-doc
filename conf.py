c225a54ee4e2cf3eb404a93b6b864737e5aa7cf9
Valentin Lorentz <progval@gmail.com>
2014-01-20 11:39:58 +0100

    Update copyright date.

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Limnoria'
-copyright = u'2003-2011, the Limnoria/Gribble/Supybot contributors'
+copyright = u'2003-2014, the Limnoria/Gribble/Supybot contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
